1089412fd1a973d2f43a4666f534d9a41d840e0a
SantiDiLiscia <santidiliscia@gmail.com>
2023-03-10 02:55:08 -0300

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+    align-items: flex-start;
+    background-color: white;
+    border-radius: 16px;
+    border: 1px solid #F4F4F4;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 8px rgba(0, 0, 0, 0.12);
+    display: flex;
+    flex-direction: column;
+    padding: 24px 32px;
+    width: 600px;
+`;
+
+export default Card;
